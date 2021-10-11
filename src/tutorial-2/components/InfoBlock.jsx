2bcdffd053ad3bdb9ea6d0e73f20b3358e778903
Archakov Dennis <amonbower@gmail.com>
2021-10-11 16:58:17 +0300

    Сделал урок №2

---
+++ src/tutorial-2/components/InfoBlock.jsx
@@ -0,0 +1,9 @@
+export const InfoBlock = () => {
+  return (
+    <p>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fuga velit, deserunt autem deleniti
+      harum amet illo quibusdam corrupti eos ipsam, excepturi perferendis iure, recusandae
+      blanditiis! Laboriosam nemo dignissimos porro blanditiis.
+    </p>
+  );
+};
